8dd19bbb372fd31d9bb637fd81cb95190661d65b
VEM27 <v.myachin2007@gmail.com>
2025-03-28 16:54:19 +0300

    Update za4et.pascal.py

--- za4et.pascal.py
+++ za4et.pascal.py
@@ -1,8 +1,13 @@
+def factorial(number):
+    result = 1
+    for multiplier in range(1, number + 1):
+        result *= multiplier
+    return result
+
 def pascal(row_index):
-    import math
     row = [0] * (row_index + 1)
     for place in range (row_index + 1):
-        row[place] = math.factorial(row_index) // (math.factorial(place) * math.factorial(row_index - place))
+        row[place] = factorial(row_index) // (factorial(place) * factorial(row_index - place))
     print(row)
 
 pascal(5)
